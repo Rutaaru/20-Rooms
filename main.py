b8b2d7c828472917203cdcc3e77bb2c2337f9150
Rutaaru <h.m.atchison1@gmail.com>
2026-03-12 14:13:04 -0700

    commit

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Name: 20 Rooms
+
+Description:
+
+Author: L.A.
+
+Update Date: 03/12/26
+"""
+
+def main():
+
+    health = int(100)
+    inventory = [0, 0, 0]
+    room = int(0)
+    match = 0
+
+    beginning
+    game_loop = True
+    while game_loop:
+        if room >= 1 and room <= 20:
+
+        else:
+            game_loop = False
+
+
+if __name__ == "__main__":
+    main()
